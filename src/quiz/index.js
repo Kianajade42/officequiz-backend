5ea73203c2160c513269111f8260debc10ac7741
Kiana Long <Kianajade42.kl@gmail.com>
2021-08-09 17:13:40 -0400

    container css and questions

---
+++ src/quiz/index.js
@@ -0,0 +1,285 @@
+const qBank = [
+  {
+    question:
+      "No one cares about your birthday.",
+    answers: ["Angela Martin","Stanley Hudson","Michael Scott","Pam Beesly"],
+    correct: "Angela Martin",
+    questionId: "1"
+  },
+    {
+    question:
+      "I talk a lot, so I’ve learned to tune myself out.",
+    answers: ["Ryan Howard","Kelly Kapoor","Kevin Malone","Oscar Martinez"],
+    correct: "Kelly Kapoor",
+    questionId: "2"
+  },
+    {
+    question:
+      "If I don’t have some cake soon, I might die.",
+    answers: ["Andy Bernard","Dwight Schrute","Angela Martin","Stanley Hudson"],
+    correct: "Stanley Hudson",
+    questionId: "3"
+  },
+    {
+    question:
+      "I am Beyoncé always.",
+    answers: ["Stanley Hudson","Michael Scott","Pam Beesly","Andy Bernard"],
+    correct: "Michael Scott",
+    questionId: "4"
+  },
+    {
+    question:
+      "I don't care what they say about me. I just want to eat.",
+    answers: ["Kevin Malone","Oscar Martinez","Michael Scott","Pam Beesly"],
+    correct: "Pam Beesly",
+    questionId: "5"
+  },
+    {
+    question:
+      "Sorry I annoyed you with my friendship.",
+    answers: ["Andy Bernard","Dwight Schrute","Jim Halpert","Robert California"],
+    correct: "Andy Bernard",
+    questionId: "6"
+  },
+    {
+    question:
+      "I’m an early bird and I’m a night owl so I’m wise and I have worms.",
+    answers: ["Michael Scott","Andy Bernard","Dwight Schrute","Creed Bratton"],
+    correct: "Michael Scott",
+    questionId: "7"
+  },
+    {
+    question:
+      "Bread is the paper of the food industry. You write your sandwich on it.",
+    answers: ["Andy Bernard","Dwight Schrute","Jim Halpert","Creed Bratton"],
+    correct: "Dwight Schrute",
+    questionId: "8"
+  },
+    {
+    question:
+      "Do I regret this? No, I believe his tissue has made me stronger. I now have the strength of a grown man and a little baby.",
+    answers: ["Andy Bernard","Dwight Schrute","Jim Halpert","Robert California",],
+    correct: "Dwight Schrute",
+    questionId: "9"
+  },
+    {
+    question:
+      "I am about to do something very bold in this job that I’ve never done before: try.",
+    answers: ["Jim Halpert","Robert California","Ryan Howard","Kelly Kapoor"],
+    correct: "Jim Halpert",
+    questionId: "10"
+  },
+    {
+    question:
+      "From time to time I send Dwight faxes. From himself. From the future.",
+    answers: ["Jim Halpert","Robert California","Ryan Howard","Pam Beesly"],
+    correct: "Jim Halpert",
+    questionId: "11"
+  },
+    {
+    question:
+      "Sold out!? That has never happened in the history of jinx!",
+    answers: ["Ryan Howard","Pam Beesly","Kevin Malone","Kelly Kapoor"],
+    correct: "Pam Beesly",
+    questionId: "12"
+  },
+    {
+    question:
+      "Do I look like someone who would waste my own time?",
+    answers: ["Robert California","Ryan Howard","Kelly Kapoor","Angela Martin"],
+    correct: "Robert California",
+    questionId: "13"
+  },
+    {
+    question:
+      "You can't have a favorite Iron Chef. It depends entirely on the secret ingredient.",
+    answers: ["Jim Halpert","Robert California","Andy Bernard","Dwight Schrute"],
+    correct: "Robert California",
+    questionId: "14"
+  },
+    {
+    question:
+      "I know a few things about love. Horrible, terrible, awful, awful things.",
+    answers: ["Michael Scott","Andy Bernard","Dwight Schrute","Ryan Howard"],
+    correct: "Andy Bernard",
+    questionId: "15"
+  },
+    {
+    question:
+      "I'm not saying I had a meteoric rise, but I did.",
+    answers: ["Phyllis Lapin-Vance","Creed Bratton","Meredith Palmer","Ryan Howard"],
+    correct: "Ryan Howard",
+    questionId: "16"
+  },
+    {
+    question:
+      "You can't let a girl feel good about herself. It will backfire on you.",
+    answers: ["Michael Scott","Creed Bratton","Andy Bernard","Ryan Howard"],
+    correct: "Andy Bernard",
+    questionId: "17"
+  },
+    {
+    question:
+      "I mean, who says exactly what they're thinking? What kind of game is that?",
+    answers: ["Angela Martin","Stanley Hudson","Kelly Kapoor","Michael Scott"],
+    correct: "Kelly Kapoor",
+    questionId: "18"
+  },
+    {
+    question:
+      "Whenever I try to make a taco, I get too excited and crush it.",
+    answers: ["Andy Bernard","Dwight Schrute","Kevin Malone","Stanley Hudson"],
+    correct: "Kevin Malone",
+    questionId: "19"
+  },
+    {
+    question:
+      "I consider myself a good person, but I'm gonna try to make him cry.",
+    answers: ["Creed Bratton","Meredith Palmer","Kevin Malone","Oscar Martinez"],
+    correct: "Oscar Martinez",
+    questionId: "20"
+  },
+    {
+    question:
+      "I love catching people in the act. That's why I always whip open doors.",
+    answers: ["Andy Bernard","Dwight Schrute","Ryan Howard","Kelly Kapoor"],
+    correct: "Dwight Schrute",
+    questionId: "21"
+  },
+    {
+    question:
+      "The man is wearing sandals. I don’t need to see Oscar’s toes at work.",
+    answers: ["Angela Martin","Stanley Hudson","Phyllis Lapin-Vance","Creed Bratton"],
+    correct: "Angela Martin",
+    questionId: "22"
+  },
+    {
+    question:
+      "I think for it to be blackmail, it would have to be a formal letter.",
+    answers: ["Michael Scott","Pam Beesly","Angela Martin","Phyllis Lapin-Vance"],
+    correct: "Phyllis Lapin-Vance",
+    questionId: "23"
+  },
+    {
+    question:
+      "You’re ugly and I know it for a fact ’cause I got the evidence right there.",
+    answers: ["Michael Scott","Pam Beesly","Angela Martin","Kelly Kapoor"],
+    correct: "Kelly Kapoor",
+    questionId: "24"
+  },
+    {
+    question:
+      "Oh, you’re paying way too much for worms. Who’s your worm guy?",
+    answers: ["Michael Scott","Creed Bratton","Oscar Martinez","Phyllis Lapin-Vance"],
+    correct: "Creed Bratton",
+    questionId: "25"
+  },
+    {
+    question:
+      "Ryan Howard is a junior sales associate at a mid-range paper supply firm.’ That’ll show ’em.",
+    answers: ["Michael Scott","Creed Bratton","Angela Martin","Ryan Howard"],
+    correct: "Ryan Howard",
+    questionId: "26"
+  },
+    {
+    question:
+      "Who is Justice Beaver?",
+    answers: ["Andy Bernard","Dwight Schrute","Michael Scott","Creed Bratton"],
+    correct: "Dwight Schrute",
+    questionId: "27"
+  },
+    {
+    question:
+      "Tell him to call me ASAP as possible.",
+    answers: ["Andy Bernard","Dwight Schrute","Michael Scott","Creed Bratton"],
+    correct: "Michael Scott",
+    questionId: "28"
+  },
+    {
+    question:
+      "I already won the lottery. I was born in the US of A, baby. And as backup, I have a Swiss passport",
+    answers: ["Andy Bernard","Stanley Hudson","Ryan Howard","Creed Bratton"],
+    correct: "Creed Bratton",
+    questionId: "29"
+  },
+    {
+    question:
+      "Tell ya one thing, I’m not gonna be a good mom tonight.",
+    answers: ["Meredith Palmer","Pam Beesly","Phyllis Lapin-Vance","Kelly Kapoor"],
+    correct: "Meredith Palmer",
+    questionId: "30"
+  },
+    {
+    question:
+      "Rit-dit-dit-do-doo!",
+    answers: ["Ryan Howard","Creed Bratton","Andy Bernard","Michael Scott"],
+    correct: "Andy Bernard",
+    questionId: "31"
+  },
+    {
+    question:
+      "Not bad for a day in the life of a dog food company.",
+    answers: ["Michael Scott","Ryan Howard","Creed Bratton","Kevin Malone"],
+    correct: "Creed Bratton",
+    questionId: "32"
+  },
+    {
+    question:
+      "I know it's illegal in Pennsylvania, but it's for charity",
+    answers: ["Michael Scott","Dwight Schrute","Creed Bratton","Kevin Malone"], 
+    correct: "Michael Scott",
+    questionId: "33"
+  },
+    {
+    question:
+      "I dont believe you, continue.",
+    answers: ["Michael Scott","Ryan Howard","Dwight Schrute","Kelly Kapoor"],
+    correct: "Dwight Schrute",
+    questionId: "34"
+  },
+    {
+    question:
+      "I hate so much about the way you choose to be.",
+    answers: ["Dwight Schrute","Robert California","Michael Scott","Meredith Palmer"],
+    correct: "Michael Scott",
+    questionId: "35"
+  },
+    {
+    question:
+      "False. I do not miss him.",
+    answers: ["Jim Halpert","Robert California","Dwight Schrute","Ryan Howard"],
+    correct: "Dwight Schrute",
+    questionId: "36"
+  },
+    {
+    question:
+      "I haven't been on a first date in 9 years.",
+    answers: ["Michael Scott","Phyllis Lapin-Vance","Pam Beesly","Meredith Palmer"],
+    correct: "Pam Beesly",
+    questionId: "37"
+  },
+    {
+    question:
+      "You should order the most expensive item on the menu so he knows you're worth it.",
+    answers: ["Michael Scott","Angela Martin","Phyllis Lapin-Vance","Dwight Schrute"],
+    correct: "Phyllis Lapin-Vance",
+    questionId: "38"
+  },
+    {
+    question:
+      "Reject a woman and she will never let it go, it's one of the many defects of their kind.",
+    answers: ["Stanley Hudson","Robert California","Michael Scott","Dwight Schrute"],
+    correct: "Dwight Schrute",
+    questionId: "39"
+  },
+    {
+    question:
+      "This isn't your grandmothers christmas party",
+    answers: ["Michael Scott","Robert California","Phyllis Lapin-Vance","Pam Beesly"],
+    correct: "Phyllis Lapin-Vance",
+    questionId: "40"
+  },
+]; 
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
