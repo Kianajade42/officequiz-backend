43efe194f90f2863df5736c1f0b986ef996e4b20
Kiana Long <Kianajade42.kl@gmail.com>
2021-08-09 18:43:41 -0400

    button function/css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDom from "react-dom";
 import "./assets/style.css";
 import  quiz from "./quiz";
+import QuestionBox from "./components/QuestionBox"
 
 class Quiz extends Component {
     state = {
@@ -21,8 +22,12 @@ class Quiz extends Component {
     render() {
         return (
         <div className="container">
-        <div className="title">The Office Quiz</div>
-        {this.state.questionBank.length > 0 && this.state.questionBank.map(({question}) => <h4>{question}</h4>) }
+        <div className="title">IT IS THE OFFICE QUIZ.</div>
+        <div className="intro"> WHO SAID IT?</div>
+        {this.state.questionBank.length > 0 && this.state.questionBank.map(
+            ({question, answers, questionId}) => (
+        <QuestionBox question={question} options={answers} key={questionId} /> ) 
+        )}
         </div> 
     )
 }
