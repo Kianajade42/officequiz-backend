d189dae7b4c8e7b302dd3eda75fc2f7f9585f8d1
Kiana Long <Kianajade42.kl@gmail.com>
2021-08-09 17:50:34 -0400

    displaying questions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
-import ReactDom from "react-dom"
+import ReactDom from "react-dom";
 import "./assets/style.css";
+import  quiz from "./quiz";
 
 class Quiz extends Component {
+    state = {
+        questionBank: []
+    };
+    getQuestions = () => {
+        quiz().then(question => {
+            this.setState({
+                questionBank: question
+            });
+
+        });
+    };
+    componentDidMount(){
+        this.getQuestions();
+    }
     render() {
         return (
         <div className="container">
         <div className="title">The Office Quiz</div>
+        {this.state.questionBank.length > 0 && this.state.questionBank.map(({question}) => <h4>{question}</h4>) }
         </div> 
     )
 }
